00acc25788c783501c3b37e5bd860f4168bcb705
pokotsun <gorigolila@gmail.com>
2018-05-21 01:57:48 +0900

    Gifファイルを作成

--- may_theory_plane.py
+++ may_theory_plane.py
@@ -31,16 +31,6 @@ def _logistic_pattern(a=4, initial_value=0.4, iterate_num=100):
 
     return rtn
 
-# アニメーション描画関数
-def _update_plot(i, logistic_points):
-    x_points = logistic_points[i]["x_points"]
-    y_points = logistic_points[i]["y_points"]
-
-    plt.plot(x_points, y_points, 'r-', lw=1)
-    #plt.title(f" Number Of Intersections ({x1}, {y1}) to ({x2}, {y2})")
-    plt.xlim([0, 1.5])
-    plt.ylim([0, 1.5])
-     
 
 # Main関数
 def main():
@@ -61,8 +51,6 @@ def main():
        initial_value = 0.4 
 
     print(f"a = {a}, Xの初期値は{initial_value}になりました。")
-    #a = sys.argv[1] or 4
-    #initial_value = sys.argv[2] or 0.4
     result_list = logistic(a=a, initial_value=initial_value)
     x_list = result_list[:-1]
     y_list = result_list[1:]
@@ -71,13 +59,12 @@ def main():
     p1, = plt.plot(x_list, y_list, "r.", markersize=0.3)
     p2, = plt.plot([0, 2], [0, 2], "g-", lw=2)
     plt.legend([p1, p2], ["logistic", "y = x"])
+    
+    plt.xlim([0, 2.0])
+    plt.ylim([0, 2.0])
     plt.title(f"a = {a}, initial value={initial_value}")
 
-    logistic_points = _logistic_pattern(a=a, initial_value=initial_value, iterate_num=200)
 
-    ani = animation.FuncAnimation(fig, _update_plot, fargs= (logistic_points,),
-        interval = 500, frames=len(logistic_points))
-    ani.save(f"logistic_period_a{a}_initvalue{initial_value}.gif", writer="imagemagick")
     plt.show()
 
 if __name__ == "__main__":
